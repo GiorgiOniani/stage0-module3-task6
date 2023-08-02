71420b4cbfea6f9344919967a4c4c081d4ffa1ce
giorgi.oniani.2 <giorgi.oniani.2@btu.edu.ge>
2023-08-02 19:42:18 +0400

    j

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int damrgvalebuli = (int) numberToBeRounded;
+        int damrgvalebuli = (int) Math.ceil(numberToBeRounded);
         System.out.println(damrgvalebuli);
     }
 }
